312268b31a0ed038574558eeb858985a769e14aa
Desh Deepak <deepakdesh565@gmail.com>
2025-08-09 13:49:42 +0000

    strings

---
+++ strings.js
@@ -0,0 +1,13 @@
+// let name = "Deepak"
+// const gameName = new String("Minecraft");
+// console.log(name[0]);
+// console.log(gameName[2]);
+
+let myDate =new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toLocaleString())
+console.log(myDate.getDay())
+console.log(myDate.getDate())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
