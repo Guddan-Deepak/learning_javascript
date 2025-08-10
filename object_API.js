a97ebc59525ecbd09ae6dc016610c1a9402aaa21
Desh Deepak <deepakdesh565@gmail.com>
2025-08-10 05:18:39 +0000

    studied object and api

---
+++ object_API.js
@@ -0,0 +1,18 @@
+//object de structure
+
+const course = {
+    coursename : "javascript",
+    price : "999"
+}
+
+const {coursename : naam} = course;
+console.log(naam);
+
+
+//json JSON stands for JavaScript Object Notation.
+// It’s a lightweight data format used for storing and exchanging data
+//  — especially between a server and a web application
+// {
+//     "name" : "deepak",
+//     "college" : "MNNIT"
+// }
--- printHello.js
+++ printHello.js
@@ -1,2 +1,6 @@
 console.log("hello vansh saini");
 
+const user = {
+    name: "deepak",
+    sub : "english"
+}
