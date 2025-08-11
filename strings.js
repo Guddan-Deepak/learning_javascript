545f2c180358223685720890248d37110a402fff
Desh Deepak <deepakdesh565@gmail.com>
2025-08-11 07:23:16 +0000

    loops,filters,map

--- strings.js
+++ strings.js
@@ -3,11 +3,33 @@
 // console.log(name[0]);
 // console.log(gameName[2]);
 
-let myDate =new Date()
-console.log(myDate)
-console.log(myDate.toString())
-console.log(myDate.toLocaleString())
-console.log(myDate.getDay())
-console.log(myDate.getDate())
-console.log(myDate.toDateString())
-console.log(myDate.toISOString())
+// let myDate =new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.getDay())
+// console.log(myDate.getDate())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+
+
+const coding =["js", "ruby","java","python","cpp"]
+
+// const values =coding.forEach( (item) => {
+//     console.log(item);          //forEach does not return any valur
+//                                 // even if return keyword is used.
+// })
+
+
+const nums=[1,2,3,4,5,6,7,8]
+const newNums =nums.filter( (num) => num>4)
+console.log(newNums);  //[ 5, 6, 7, 8 ]
+
+
+const newarr =nums.filter( (num) => {
+    return num>4                //we have to use return keyword to explicitly
+                                //return the value
+})
+console.log(newarr);  //[ 5, 6, 7, 8 ]
+
+nums.map()
